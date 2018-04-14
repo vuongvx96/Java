563d432d46311c2b07e003cd5439f6431f24f6f9
Vuongvx96 <vuongvx@hotmail.com>
2018-04-14 16:29:52 +0700

    Update bai 69 -> 79

---
+++ GUI/src/Bai69/Program.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bai69;
+
+/**
+ *
+ * @author Louis
+ */
+public class Program {
+    public static void main(String[] args){
+        new frmManHinhTong2SoNguyen().setVisible(true);
+        
+    }
+}
